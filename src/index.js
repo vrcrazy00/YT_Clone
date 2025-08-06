dd801157ec7d1de2f7e8fc656d428ea933a2f00f
vrcrazy00 <vrcrazy00@gmail.com>
2025-08-06 20:48:56 +0530

    comitt

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import dotenv from "dotenv"
 dotenv.config({path:'./env'})
 import connectDB from "./db/index.js"
 
-
+import {app} from "./app.js"
 
 
 
 connectDB();
-
+then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is runing......`);
+        
+    })
+})
 
 
 
